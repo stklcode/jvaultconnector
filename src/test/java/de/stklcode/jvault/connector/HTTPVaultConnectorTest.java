ffdc0f6a2703a49dd76d671305b3ec549e261f51
Stefan Kalscheuer <stefan@stklcode.de>
2019-10-03 11:09:39 +0200

    Test against Vault 1.2.2

--- src/test/java/de/stklcode/jvault/connector/HTTPVaultConnectorTest.java
+++ src/test/java/de/stklcode/jvault/connector/HTTPVaultConnectorTest.java
@@ -54,7 +54,7 @@ import static org.junit.jupiter.api.Assumptions.assumeTrue;
  */
 @Tag("online")
 public class HTTPVaultConnectorTest {
-    private static String VAULT_VERSION = "1.2.2";  // the vault version this test is supposed to run against
+    private static String VAULT_VERSION = "1.2.3";  // the vault version this test is supposed to run against
     private static final String KEY1 = "E38bkCm0VhUvpdCKGQpcohhD9XmcHJ/2hreOSY019Lho";
     private static final String KEY2 = "O5OHwDleY3IiPdgw61cgHlhsrEm6tVJkrxhF6QAnILd1";
     private static final String KEY3 = "mw7Bm3nbt/UWa/juDjjL2EPQ04kiJ0saC5JEXwJvXYsB";
