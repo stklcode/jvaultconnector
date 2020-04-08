edb9194153697cbb9dbfe9f589ed18ad352a55f8
Stefan Kalscheuer <stefan@stklcode.de>
2020-04-08 14:23:48 +0200

    Update copyright notice to 2020
    Better late than never, at least before the next release... [skip ci]

--- src/main/java/de/stklcode/jvault/connector/model/response/embedded/TokenData.java
+++ src/main/java/de/stklcode/jvault/connector/model/response/embedded/TokenData.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2019 Stefan Kalscheuer
+ * Copyright 2016-2020 Stefan Kalscheuer
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
